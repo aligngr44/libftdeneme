67b631e29fe09dedb9d9237f5908ad34120d2d1f
Ali Gungor <algungor@k1m27s07.42istanbul.com.tr>
2026-01-09 18:37:19 +0300

    memset int olarak çalıştırma

--- ft_memset.c
+++ ft_memset.c
@@ -21,10 +21,17 @@ void *ft_memset(void *p, int c, size_t n)
     }
     return x;
 } 
-int main()
+/*int main()
 {
     char a[] = "asdewq";
     char c = 'x';
     ft_memset(a, c, 3);
     printf("%s\n", a);
+}/*/
+int main()
+{
+    int a = 0;
+    ft_memset(&a, 154, 4);
+    ft_memset(&a, 0, 1);
+    printf("%d\n", a);
 }
